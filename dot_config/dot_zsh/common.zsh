fa9e1f57ce6d33e8fbbe9ffc140ae00b667c3576
nozomemein <b8yukifsukeo999n@gmail.com>
2026-02-02 09:20:31 +0900

    .

---
+++ dot_config/dot_zsh/common.zsh
@@ -0,0 +1,14 @@
+# ---- helpers ----
+path_prepend() { [[ -d "$1" ]] && path=("$1" $path); }
+path_append()  { [[ -d "$1" ]] && path+=("$1"); }
+
+# zsh's path array: unique
+typeset -U path PATH
+
+# ---- aliases (common) ----
+alias g='git'
+alias ls='eza'
+alias ll='ls -l'
+alias gl='g log --oneline'
+alias cz='chezmoi'
+# alias cat='bat'
