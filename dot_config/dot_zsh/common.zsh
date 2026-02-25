bdbaa9cd36a99afdda4bf1ad1c22b219f7ee17ff
nozomi.hijikata <nozomi.hijikata@techouse.jp>
2026-02-25 18:02:25 +0900

    .

--- dot_config/dot_zsh/common.zsh
+++ dot_config/dot_zsh/common.zsh
@@ -20,3 +20,8 @@ alias cat='bat'
 fpath+=("$(brew --prefix)/share/zsh/site-functions")
 autoload -U promptinit; promptinit
 prompt pure
+
+# bind fix
+bindkey '^R' history-incremental-search-backward
+bindkey '^A' beginning-of-line
+bindkey '^E' end-of-line
