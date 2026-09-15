a14009f280325978feb6030f8145770a2ec58b42
Nozomi-Hijikata <nozomi.hijikata@techouse.jp>
2026-09-15 11:26:15 +0900

    nvim: pin nvim-treesitter

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- Pin to master: NvChad's config uses nvim-treesitter.configs (master API).
+  -- The main branch is a rewrite that drops that module; switch only once NvChad supports it.
+  branch = "master",
   opts = {
     ensure_installed = {
       "vim",
